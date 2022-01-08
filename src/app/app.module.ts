91cbf801c71f0b4f4f421d783edef29b5c6e8266
Jane Njoroge <jane.njoroge@student.moringaschool.com>
2022-01-08 18:02:04 +0300

    add header component and details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { QuotesComponent } from './quotes/quotes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    QuotesComponent
   ],
   imports: [
     BrowserModule,
