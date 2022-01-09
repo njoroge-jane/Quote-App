e4c9a2a7fd69ea0f5bdaf426d3af02bf9a242643
Jane Njoroge <jane.njoroge@student.moringaschool.com>
2022-01-09 16:57:49 +0300

    indent my components

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -1,32 +1,29 @@
-import { Component, Input, OnInit, Output, EventEmitter} from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { Quote } from '../quote';
-
 
 @Component({
   selector: 'app-quote-detail',
   templateUrl: './quote-detail.component.html',
-  styleUrls: ['./quote-detail.component.css']
+  styleUrls: ['./quote-detail.component.css'],
 })
 export class QuoteDetailComponent implements OnInit {
-@Input() quote!:Quote;
-@Output() deleteThis= new EventEmitter<boolean>();
+  @Input() quote!: Quote;
+  @Output() deleteThis = new EventEmitter<boolean>();
 
-deleteQuote(remove:boolean){
-this.deleteThis.emit(remove)
-}
- voteUp = 0;
- voteDown = 0;
- 
- votingUp(){
-this.voteUp++;
- }
- votingDown(){
-  this.voteDown++;
-   }
+  deleteQuote(remove: boolean) {
+    this.deleteThis.emit(remove);
+  }
+  voteUp = 0;
+  voteDown = 0;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  votingUp() {
+    this.voteUp++;
+  }
+  votingDown() {
+    this.voteDown++;
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,12 @@
 export class Quote {
-moreDetails:boolean;
-constructor(public quote:string, public author:string, public submittedBy:string){
-this.moreDetails=false;
-}
+  moreDetails: boolean;
+  constructor(
+    public quote: string,
+    public author: string,
+    public submittedBy: string
+  ) {
+    this.moreDetails = false;
+  }
+  voteUp: number = 0;
+  voteDown: number = 0;
 }
